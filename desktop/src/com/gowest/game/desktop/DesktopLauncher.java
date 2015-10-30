93692028fe5eee04b19c9cc20281b9c1244600f3
Tobias Friedinger <frostig@acidrain.eu>
2015-10-30 16:16:47 +0100

    libGDX Setup
    Adding libGDX Setup generated files.

---
+++ desktop/src/com/gowest/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gowest.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gowest.game.GoWestMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GoWestMain(), config);
+	}
+}
